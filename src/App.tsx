6dfa146c5efaa17c52bbdeb529cbda9d638eb021
dshukertjr <18113850+dshukertjr@users.noreply.github.com>
2020-09-08 18:24:16 +0900

    edited theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import './App.css';
+import Button from '@material-ui/core/Button';
 
 function App() {
-  return <div>aaa</div>;
+  return (
+    <Button variant="contained" color="primary">
+      Hello World
+    </Button>
+  );
 }
 
 export default App;
---
+++ src/theme.tsx
@@ -0,0 +1,11 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+import { blue, red, orange } from '@material-ui/core/colors';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: orange,
+    secondary: blue,
+  },
+});
+
+export default theme;
