2e0c82c894fdb1508947dd1a23c5931f2ad9c6fb
dshukertjr <18113850+dshukertjr@users.noreply.github.com>
2020-09-09 17:01:22 +0900

    created text form

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import TextField from '@material-ui/core/TextField';
 
 function Home() {
-  return <div>Home works!</div>;
+  return <TextField id="outlined-basic" label="Outlined" variant="outlined" />;
 }
 
 export default Home;
